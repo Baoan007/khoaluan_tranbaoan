2799063cb493b0c7e1611bc024023dc1f79f366a
khuong <vankhuong240499@gmail.com>
2023-05-23 10:22:35 +0700

    U

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 
 from flask import Flask
-from flask_cors import CORS
+# from flask_cors import CORS
 from flask import Flask, render_template, jsonify, request, abort
 import os
 # from rasa_api import model_path, register_bot_dev, call_rasa_model
