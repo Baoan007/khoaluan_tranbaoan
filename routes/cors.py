14ecd57230770134b9d8699633a6b4bcc2d0fb4c
khuong <vankhuong240499@gmail.com>
2023-05-29 12:38:08 +0700

    U

--- routes/cors.py
+++ routes/cors.py
@@ -10,4 +10,4 @@ def register_cors(app):
     # Cấu hình CORS
     csrf.init_app(app)
     CORS(app, origins=['http://127.0.0.1:8000', 'http://localhost:5055', 'http://localhost:5005', 'http://127.0.0.1:5000', 'http://khtntuyensinh.autos',
-         'http://khtntuyensinh.info'])
+         'http://khtntuyensinh.info', 'http://0.0.0.0:5055'])
