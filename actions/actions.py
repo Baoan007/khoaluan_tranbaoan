1750bbffc3e827353eb94021f686d8e211febe3d
Baoan007 <tranbaoanfetel@gmail.com>
2023-06-30 20:26:17 +0700

    30-Jun

--- actions/actions.py
+++ actions/actions.py
@@ -1,12 +1,5 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
 
-
-# This is a simple example for a custom action which utters "Hello World!"
-
+from tkinter import EventType
 from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker
@@ -14,10 +7,33 @@ from rasa_sdk.executor import CollectingDispatcher
 #
 #
 
-import requests
-import json
 
-import re
-from rasa_sdk.events import SlotSet, AllSlotsReset
-from utils.utils import format_amount
-from config import app_config
+from rasa_sdk.events import SlotSet
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine('mysql+pymysql://root:@localhost/my_khtn')
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
+class ActionTriggerResponseSelector(Action):
+    """Returns the chatchit utterance dependent on the intent"""
+
+    def name(self) -> Text:
+        return "action_trigger_response_selector"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[EventType]:
+        retrieval_intent = tracker.get_slot("retrieval_intent")
+        if retrieval_intent:
+            dispatcher.utter_message(template=f"utter_{retrieval_intent}")
+
+        return [SlotSet("retrieval_intent", None)]
+
+    
